af27c60d341bcf135d527054d33d34615027c8b5
daren866 <105366647+daren866@users.noreply.github.com>
2026-02-13 18:40:39 +0800

    初次上传文件

---
+++ Project.swift
@@ -0,0 +1,30 @@
+import ProjectDescription
+
+let deploymentTargets: DeploymentTargets = .iOS("13.0")
+
+let project = Project(
+    name: "Gudaobaiban",
+    organizationName: "Example",
+    settings: .settings(
+        base: [
+            "DEVELOPMENT_TEAM": "<YOUR_DEVELOPMENT_TEAM_ID>",
+            "CODE_SIGN_STYLE": "Automatic",
+            "CODE_SIGN_IDENTITY": "Apple Development",
+            "TARGETED_DEVICE_FAMILY": "1",
+            "ENABLE_USER_SCRIPT_SANDBOXING": "NO"
+        ],
+        defaultSettings: .recommended
+    ),
+    targets: [
+        .target(
+            name: "Gudaobaiban",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "com.gudao.webapp.gudaobaiban",
+            deploymentTargets: deploymentTargets,
+            infoPlist: .file(path: "Gudaobaiban/Info.plist"),
+            sources: ["Gudaobaiban/**/*.swift"],
+            resources: ["Gudaobaiban/Assets.xcassets", "Gudaobaiban/index.html"]
+        )
+    ]
+)
